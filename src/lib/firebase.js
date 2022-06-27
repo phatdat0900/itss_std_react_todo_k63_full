a08f8c94567bbc942d4d9f3646b6c2f9dad627f3
datletan <>
2022-06-27 04:09:20 +0000

    Save Todo on Firestore

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,19 +3,65 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAU86vilLsKCEwMugzG-hwn0EizlGuGRo4",
-  authDomain: "itss-53d01.firebaseapp.com",
-  projectId: "itss-53d01",
-  storageBucket: "itss-53d01.appspot.com",
-  messagingSenderId: "394000956482",
-  appId: "1:394000956482:web:d9837a1899f7e5db2a81db"
+  apiKey: "AIzaSyAtNsU-2sH3hEpon4fUmsdYuPqIx1DFldk",
+  authDomain: "itss-2.firebaseapp.com",
+  projectId: "itss-2",
+  storageBucket: "itss-2.appspot.com",
+  messagingSenderId: "1028586956613",
+  appId: "1:1028586956613:web:07b1b87532545d4f6d402c"
 };
 
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+export const auth = firebase.auth();
+export default firebase;
+
+export const getFirebaseItems = async () => {
+  try {
+    const snapshot = await db
+      .collection("todos")
+      .get();
+    const items = snapshot.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id })
+    );
+    return items;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
+
+export const addFirebaseItem = async (item) => {
+  try {
+    const todoRef = db.collection("todos");
+    await todoRef.add(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const updateFirebaseItem = async (item, id) => {
+  try {
+    const todoRef = db.collection("todos").doc(id);
+    await todoRef.update(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const clearFirebaseItem = async (item) => {
+  const todoRef = db.collection("todos").doc(item.id);
+  await todoRef.delete().then(function () {
+  }).catch(function (err) {
+    console.log(err);
+  });
+};
